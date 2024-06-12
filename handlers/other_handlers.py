69152200dc72770c2b9d635afccd4dfb71992e46
Oscardk <dauren2050@yandex.ru>
2024-06-12 18:57:02 +0500

    fix

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -10,4 +10,12 @@ router = Router()
 async def sedn_echo(message: Message):
    await message.send_copy(chat_id=message.chat.id)
    await message.answer(text='Вот клавиатура', reply_markup=kb_builder.as_markup(resize_keyboard=True))
+   #допиши сюда код, который будет отправлять сообщение обратно пользователю
+   #с клавиатурой kb_builder
+   #подсказка: используй метод send_copy у объекта message
+   #подсказка: используй метод answer у объекта message  
+   #подсказка: используй параметр reply_markup у метода answer
+   #подсказка: используй параметр as_markup у объекта kb_builder
+
+   
    
